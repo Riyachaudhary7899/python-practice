c2c206c5d9803f98bd3e9b3ecf5e7e9d1eb689b9
Riya <riyadengree189251@gmail.com>
2026-02-01 10:44:41 +0530

    Create 1_variables.py

---
+++ 1_variables.py
@@ -0,0 +1,12 @@
+# 1_variables.py
+# This program explains variables and data types in Python
+
+name = "Riya"          # string
+age = 20               # integer
+height = 5.2           # float
+is_student = True      # boolean
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
